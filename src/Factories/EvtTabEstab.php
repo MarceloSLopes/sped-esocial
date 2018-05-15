99142576ee00b7d83772cff762416f512e661240
MarceloSLopes <marcelo.s.lopez@gmail.com>
2018-05-15 15:42:01 -0300

    Update EvtTabEstab.php
    Correção de erro ao enviar o evento no modo alteração.

--- src/Factories/EvtTabEstab.php
+++ src/Factories/EvtTabEstab.php
@@ -305,13 +305,13 @@ class EvtTabEstab extends Factory implements FactoryInterface
             $newVal       = $this->std->novavalidade;
             $novaValidade = $this->dom->createElement("novaValidade");
             $this->dom->addChild(
-                $ideRubrica,
+                $novaValidade,
                 "iniValid",
                 $newVal->inivalid,
                 true
             );
             $this->dom->addChild(
-                $ideRubrica,
+                $novaValidade,
                 "fimValid",
                 ! empty($newVal->fimvalid) ? $newVal->fimvalid : null,
                 false
