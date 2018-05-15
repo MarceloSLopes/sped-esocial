78f1c4172e334093db13d0d23937e8c4ae2275ce
MarceloSLopes <marcelo.s.lopez@gmail.com>
2018-05-15 15:40:43 -0300

    Update EvtTabProcesso.php
    Correção de erro ao enviar o evento no modo alteração.

--- src/Factories/EvtTabProcesso.php
+++ src/Factories/EvtTabProcesso.php
@@ -207,13 +207,13 @@ class EvtTabProcesso extends Factory implements FactoryInterface
             $newVal       = $this->std->novavalidade;
             $novaValidade = $this->dom->createElement("novaValidade");
             $this->dom->addChild(
-                $ideRubrica,
+                $novaValidade,
                 "iniValid",
                 $newVal->inivalid,
                 true
             );
             $this->dom->addChild(
-                $ideRubrica,
+                $novaValidade,
                 "fimValid",
                 ! empty($newVal->fimvalid) ? $newVal->fimvalid : null,
                 false
